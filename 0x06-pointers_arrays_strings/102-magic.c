d371734d6852ca80691b3ac762c647db4b26ea3c
raudaholamide <raudaholamide@gmail.com>
2023-03-02 15:20:34 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,6 +9,8 @@ int main(void);
 	p = &n;
   /*
    * write your line of code here...
+   * Remember
+   * main = magic
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
